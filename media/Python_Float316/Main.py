1c800c167da0e74bcb0bb1f23930318c364e4b02
Norbert Hantos <66911274+HantosNorbert@users.noreply.github.com>
2023-09-18 14:57:55 +0200

    Delete media/Python_Float316 directory

--- media/Python_Float316/Main.py
+++
@@ -1,6 +0,0 @@
-from CalcPixel import CalcPixel
-from ImageRenderer import ImageRenderer
-
-if __name__ == "__main__":
-    CalcPixel.init()
-    ImageRenderer.render()
--- media/Python_Float316/Surface.py
+++
@@ -1,9 +0,0 @@
-# A class for surface objects defined at a specific point:
-# contains a signed distance from the closest object and its
-# color. Technically a Vec2 with a specific purpose.
-from Float316 import Float316
-
-class Surface:
-    def __init__(self, sd: Float316, col: Float316):
-        self.sd = sd
-        self.col = col
