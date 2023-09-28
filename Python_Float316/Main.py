53f62975158933215c84c989cc0eb20085e8b387
Norbert Hantos <66911274+HantosNorbert@users.noreply.github.com>
2023-09-28 13:42:23 +0200

    Update Main.py

--- Python_Float316/Main.py
+++ Python_Float316/Main.py
@@ -1,3 +1,9 @@
+// ################################################################################
+// # A raytracer created by Norbert Hantos
+// #
+// # See https://github.com/HantosNorbert/Raymarching-in-Jack
+// ################################################################################
+
 from CalcPixel import CalcPixel
 from ImageRenderer import ImageRenderer
 
